c8c78495571b13e9517dad340b03cbbcee4e2b33
Caju Kaiju <caju.kaiju@gmail.com>
2024-09-16 21:43:00 -0700

    Grid created

--- cgol/bin/main.ml
+++ cgol/bin/main.ml
@@ -1,65 +1,4 @@
-[@@@ocaml.warning "-69-27-32-34-50"]
-
-(* let random_color () = *)
-(*   let r = Raylib.get_random_value 0 255 in *)
-(*   let g = Raylib.get_random_value 0 255 in *)
-(*   let b = Raylib.get_random_value 0 255 in *)
-(*   Raylib.Color.create r g b 255 *)
-(* ;; *)
-(**)
-(* module Window = struct *)
-(*   let width = 1200 *)
-(*   let height = 1200 *)
-(*   let bg_color = Raylib.Color.create 255 255 255 255 *)
-(**)
-(*   let setup () = *)
-(*     Raylib.init_window width height "Boxbounce"; *)
-(*     Unix.gettimeofday () |> truncate |> Unsigned.UInt.of_int |> Raylib.set_random_seed; *)
-(*     Raylib.set_target_fps 60; *)
-(*     () *)
-(*   ;; *)
-(* end *)
-(**)
-(* module Tile = struct *)
-(*   type t = *)
-(*     { size : int *)
-(*     ; xpos : int *)
-(*     ; ypos : int *)
-(*     } *)
-(**)
-(*   let create xpos ypos size = { size; xpos; ypos } *)
-(*   let draw t = Raylib.draw_rectangle t.xpos t.ypos t.size t.size (random_color ()) *)
-(* end *)
-(**)
-(* module Grid = struct *)
-(*   type t = *)
-(*     { htile_count : int *)
-(*     ; wtile_count : int *)
-(*     ; tiles : Tile.t list list *)
-(*     } *)
-(**)
-(*   let draw t = t.tiles |> List.flatten |> List.iter Tile.draw *)
-(**)
-(*   let create height width size = *)
-(*     let hcount = height / size in *)
-(*     let wcount = width / size in *)
-(*     let tiles = *)
-(*       List.init hcount (fun j -> List.init wcount (fun i -> Tile.create (i * size) (j * size) size)) *)
-(*     in *)
-(*     { htile_count = hcount; wtile_count = wcount; tiles } *)
-(*   ;; *)
-(* end *)
-(**)
-(* let rec loop grid = *)
-(*   match Raylib.window_should_close () with *)
-(*   | true -> Raylib.close_window () *)
-(*   | false -> *)
-(*     Raylib.begin_drawing (); *)
-(*     Raylib.clear_background Window.bg_color; *)
-(*     Grid.draw grid; *)
-(*     Raylib.end_drawing (); *)
-(*     loop grid *)
-(* ;; *)
+[@@@ocaml.warning "-69-27-32-37-34-50"]
 
 let color_black = Raylib.Color.create 255 255 255 255
 let color_white = Raylib.Color.create 0 0 0 255
@@ -91,6 +30,16 @@ module Tile = struct
 
   let dead_color = color_black
   let alive_color = color_white
+
+  let create xpos ypos size state =
+    let color =
+      match state with
+      | Dead -> dead_color
+      | Alive -> alive_color
+    in
+    { xpos; ypos; size; color; state }
+  ;;
+
   let draw t = Raylib.draw_rectangle t.xpos t.ypos t.size t.size t.color
 end
 
@@ -100,19 +49,36 @@ module Grid = struct
     ; ncols : int
     ; tiles : Tile.t list
     }
+
+  let draw t = List.iter Tile.draw t.tiles
+
+  let create nrows ncols tile_size =
+    let tiles =
+      List.init nrows (fun r ->
+        List.init ncols (fun c -> Tile.create (c * tile_size) (r * tile_size) tile_size Tile.Dead))
+      |> List.flatten
+    in
+    { nrows; ncols; tiles }
+  ;;
 end
 
-let rec loop () =
+let rec loop grid =
   match Raylib.window_should_close () with
   | true -> Raylib.close_window ()
   | false ->
+    let new_grid = grid in
     Raylib.begin_drawing ();
     Raylib.clear_background Window.bg_color;
+    Grid.draw new_grid;
     Raylib.end_drawing ();
-    loop ()
+    loop new_grid
 ;;
 
 let () =
+  let size = 5 in
+  let nrows = Window.height / size in
+  let ncols = Window.width / size in
+  let grid = Grid.create nrows ncols size in
   Window.setup ();
-  loop ()
+  loop grid
 ;;
